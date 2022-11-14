97b052c987d08f6e545da15e1f18eafe3c39a9dd
eschuman20 <93109735+eschuman20@users.noreply.github.com>
2022-11-13 20:33:53 -0500

    Update project2question3.py
    Added comments

--- project2question3.py
+++ project2question3.py
@@ -20,38 +20,47 @@ conf = (conf.setMaster('local[*]')
 sc = SparkContext(conf = conf)
 Spark = SparkSession(sc)
 
-
+#Create transactions dataframe
 t_df = Spark.read.csv("C:\\Users\\eschu\\OneDrive\\Documents\\EricsStuff\\DS503\\project2.3\\transactions.csv",inferSchema=True)
 
+#1: Filter out (drop) the transactions from T whose total amount is less than $200
 T1 = t_df.select(t_df["_c0"].alias("TransID"),
                  t_df["_c1"].alias("CustID"),
                  t_df["_c2"].alias("TransTotal"),
                  t_df["_c3"].alias("TransNumItems"),
                  t_df["_c4"].alias("TransDesc")).where(t_df["_c2"] >= 200)
-T1.show()
 
+#2: Over T1, group the transactions by the Number of Items it has, and for each group
+#calculate the sum of total amounts, the average of total amounts, and the min and the
+#max of the total amounts.
 T2 = T1.groupBy(T1["TransNumItems"].alias("Num_Items")).agg(sum("TransTotal").alias("SumTransTotal"), 
                                                   avg("TransTotal").alias("AvgTransTotal"), 
                                                   min("TransTotal").alias("MinTransTotal"), 
                                                   max("TransTotal").alias("MaxTransTotal"))
+#3: Report back T2 to the client side
 T2.show()
 
+#4:Over T1, group the transactions by customer ID, and for each group report the
+#customer ID and the transactions’ count
 T3 = T1.groupBy(T1["CustID"].alias("CustID")).agg(count(T1["TransID"]).alias("TransactionsCount"))
-T3.show()
 
+#5: Filter out (drop) the transactions from T whose total amount is less than $600
 T4 = t_df.select(t_df["_c0"].alias("TransID"),
                  t_df["_c1"].alias("CustID"),
                  t_df["_c2"].alias("TransTotal"),
                  t_df["_c3"].alias("TransNumItems"),
                  t_df["_c4"].alias("TransDesc")).where(t_df["_c2"] >= 600)
-T4.show()
 
+#6: Over T4, group the transactions by customer ID, and for each group report the
+#customer ID, and the transactions’ count
 T5 = T4.groupBy(T4["CustID"].alias("CustID")).agg(count(T4["TransID"]).alias("TransactionsCount"))
-T5.show()
 
-#join does not return anything
+
+#7: Select the customer IDs whose T5.count * 5 < T3.count 
 T6 = T5.join(T3, T5["CustID"] == T3["CustID"]).select((T5["CustID"]), (T5["TransactionsCount"].alias("Count_T5")), 
                 (T3["TransactionsCount"].alias("Count_T3"))).where((T5["TransactionsCount"] * 5) < T3["TransactionsCount"])
+#8. Report back T6 to the client side
+#join does not return anything because there are no
 T6.show()
 
 
